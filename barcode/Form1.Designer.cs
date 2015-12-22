219b35ea86caae5cda49731fb20da4fde4209093
mintoo511 <mintoo511@gmail.com>
2015-12-23 03:55:26 +0530

    adding files

---
+++ barcode/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Genarate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Genarate
+            // 
+            this.Genarate.Location = new System.Drawing.Point(274, 37);
+            this.Genarate.Name = "Genarate";
+            this.Genarate.Size = new System.Drawing.Size(104, 29);
+            this.Genarate.TabIndex = 0;
+            this.Genarate.Text = "Generate";
+            this.Genarate.UseVisualStyleBackColor = true;
+            this.Genarate.Click += new System.EventHandler(this.Genarate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 90);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(412, 236);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Genarate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Barcode Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Genarate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ barcode/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+namespace barcode
+{
+    public partial class Form1 : Form
+    {
+        int flag = 0;
+        string barcode; 
+        Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Genarate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                barcode = textBox1.Text;
+                bitmap = new Bitmap(barcode.Length * 40,50);
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    Font ofont = new System.Drawing.Font("IDAutomationHC39M", 20);
+                    PointF point = new PointF(2f, 2f);
+                    SolidBrush black = new SolidBrush(Color.Black);
+                    SolidBrush white = new SolidBrush(Color.White);
+                    graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
+                    graphics.DrawString("*" + barcode + "*", ofont, black, point);
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+                    pictureBox1.Image = bitmap;
+                    pictureBox1.Height = bitmap.Height;
+                    pictureBox1.Width = bitmap.Width;
+                }
+                //   bitmap.Save("" +barcode + ".Jpeg");
+                flag = 1;
+            }
+            catch
+            {
+                MessageBox.Show("Enter String");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // int flag=0;
+            if (flag == 0)
+            {
+                MessageBox.Show("Generate Barcode First");
+            }
+            else if (flag == 1)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Title = "Save BarCode";
+                saveFileDialog1.FileName = barcode;
+                saveFileDialog1.DefaultExt = "png";
+                saveFileDialog1.Filter = "jpeg (*.jpeg)|*.jpeg|png (*.png)|*.png|gif(*.gif)|*.gif|bmp (*.bmp)|*.bmp|tiff (*.tiff)|*.tiff";
+                string name="image";
+                string ext = "ext";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                         name = saveFileDialog1.FileName;
+                         ext = System.IO.Path.GetExtension(saveFileDialog1.FileName);
+                         bitmap.Save(name);
+                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine("error 404");
+                    }
+                }
+               
+            } 
+            else
+            {
+                MessageBox.Show("Unknown Error");
+            }
+        }
+ 
+    }
+}
